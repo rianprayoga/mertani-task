c6845a2ddb5fbe44661b52902dedaed0297596dc
rian <junianto.riansp@gmail.com>
2025-12-05 14:37:13 +0700

    chore: connect db

--- device-service/cmd/api/main.go
+++ device-service/cmd/api/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+)
+
+type application struct {
+	DSN      string
+	HttpPort string
+}
 
 func main() {
-	fmt.Println("tes")
+
+	var app application
+
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=mertani password=mertani dbname=mertani_devices timezone=UTC", "Postgres connection string")
+	flag.StringVar(&app.HttpPort, "http-port", "8081", "Port for http inventories service")
+	flag.Parse()
+
+	conn, err := app.connectDb()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
 }
